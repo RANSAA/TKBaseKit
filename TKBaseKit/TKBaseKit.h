b88c7755bfeda2908a2a20107f7332a31f086a82
RANSAA <1352892108@qq.com>
2024-10-05 04:45:21 +0800

    fix header

--- TKBaseKit/TKBaseKit.h
+++ TKBaseKit/TKBaseKit.h
@@ -6,10 +6,23 @@
 //  Copyright © 2019 mac. All rights reserved.
 //
 
-#ifndef TKBaseKit_h
-#define TKBaseKit_h
+//#ifndef TKBaseKit_h
+//#define TKBaseKit_h
+//
+//#import <TKSDKUniversal/TKSDKUniversal.h>
+//#import <TKSDKTool/TKSDKTool.h>
+//
+//#endif /* TKBaseKit_h */
+
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TKBaseKit.
+FOUNDATION_EXPORT double TKBaseKitVersionNumber;
+//! Project version string for TKBaseKit.
+FOUNDATION_EXPORT const unsigned char TKBaseKitVersionString[];
+
+
 
 #import <TKSDKUniversal/TKSDKUniversal.h>
 #import <TKSDKTool/TKSDKTool.h>
-
-#endif /* TKBaseKit_h */
